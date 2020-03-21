a1f4e1ece9ea6027c7e523d079ede0e257553fc7
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-21 17:38:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,17 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+  
+  get '/' do
+    "My name is Cat."
+  end
+  
+  get '/' do
+    "My hometown is Lawrenceville."
+  end
+  
+  get '/' do
+    "My favorite song is '"
+  end
+  
 end
