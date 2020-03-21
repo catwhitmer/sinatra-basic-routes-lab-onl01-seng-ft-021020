fe044ed01b1f4ab63c96e4e211dbc992f923d42e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-21 17:44:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Cat."
+  end
+  
+  get '/hometown' do
+    "My hometown is Lawrenceville."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'La vie en Rose', by Louis Armstrong."
+  end
+  
 end
